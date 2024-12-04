ecb16673a144fd54a761296ae8ed9183aa02a0ce
Deepak <dev_rep@probeplus.in>
2024-12-04 09:18:55 +0530

    added few answers

--- Assignment-2/main.py
+++ Assignment-2/main.py
@@ -26,9 +26,107 @@ class Solution(object):
 
 
 # 2. https://leetcode.com/problems/running-sum-of-1d-array/
+
+
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        total = 0
+        for num in range(len(nums)):
+            total += nums[num]
+            nums[num] = total
+        return nums
+        
+        
+        
+        
 # 3. https://leetcode.com/problems/jewels-and-stones
+
+
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        out = 0
+        for n in stones:
+            if n in jewels:
+                out += 1
+
+        return out
+                
+
+        
+
 # 4. https://leetcode.com/problems/minimum-absolute-difference
+
 # 5. https://leetcode.com/problems/three-consecutive-odds
+
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        odd = []
+        out = False
+        for n in arr:
+            if n%2 != 0 and n != 2:
+                odd.append(n)
+                if len(odd) == 3:
+                    out = True
+                    break
+            else:
+                odd = []
+        return out
+        
 # 6. https://leetcode.com/problems/transpose-matrix
+
 # 7. https://leetcode.com/problems/majority-element
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        value = len(nums) / 2.0
+        datalist = []
+        for n in nums:
+            if n not in datalist:
+                datalist.append(n)
+                counted = nums.count(n)
+                if counted > value:
+                    return n
+
+
 # 8. https://leetcode.com/problems/move-zeroes
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        non_zero = 0
+        for n in range(len(nums)):
+            if nums[n] != 0:
+                nums[non_zero] = nums[n]
+                non_zero += 1
+
+        for i in range(non_zero, len(nums)):
+            nums[i] = 0 
+            
+        return nums
+
+
+                
+
+
+
+        
